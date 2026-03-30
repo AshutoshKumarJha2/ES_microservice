124e230207980881d8ebb0dba8bc13b184e35f61
Jha <2479623@cognizant.com>
2026-03-30 11:53:45 +0530

    feature: added api spec inside backend/ | fix: issues with .gitignores

---
+++ backend/api-gateway/~launch.ps1
@@ -0,0 +1,1 @@
+Set-Location 'C:\Users\2479623\Desktop\ES_microservice\backend\api-gateway'; .\mvnw spring-boot:run
---
+++ backend/config-server/~launch.ps1
@@ -0,0 +1,1 @@
+Set-Location 'C:\Users\2479623\Desktop\ES_microservice\backend\config-server'; .\mvnw spring-boot:run
